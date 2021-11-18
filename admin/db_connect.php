251e4ddd8db3b0c0c6d01d3fc1e93c5e2b2b0c66
Skylahaustine <austineskylah9195@gmail.com>
2021-11-18 17:54:48 +0300

    remote database

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,4 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','eventdb')or die("Could not connect to mysql".mysqli_error($con));
+//$conn= new mysqli('localhost','root','','eventdb')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('remotemysql.com','d59XO4qkfF','jmVRCpdSbG','d59XO4qkfF')or die("Could not connect to mysql".mysqli_error($con));

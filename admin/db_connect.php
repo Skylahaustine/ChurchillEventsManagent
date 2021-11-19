d1161ca41e5a1a5630fdc9a2a756df331910d45d
Skylahaustine <austineskylah9195@gmail.com>
2021-11-19 08:49:07 +0300

    db2

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,4 +1,6 @@
 <?php 
 
 //$conn= new mysqli('localhost','root','','eventdb')or die("Could not connect to mysql".mysqli_error($con));
-$conn= new mysqli('remotemysql.com','d59XO4qkfF','jmVRCpdSbG','d59XO4qkfF')or die("Could not connect to mysql".mysqli_error($con));
+//$conn= new mysqli('remotemysql.com','d59XO4qkfF','jmVRCpdSbG','d59XO4qkfF')or die("Could not connect to mysql".mysqli_error($con));
+//VERSION 2
+$conn= new mysqli('remotemysql.com','cOq1reHwxl','H1iYEFor7j','cOq1reHwxl')or die("Could not connect to mysql".mysqli_error($con));

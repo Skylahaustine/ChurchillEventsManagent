f3a0da073d56ff970c1a78f1aef746c4084a9425
Skylahaustine <austineskylah9195@gmail.com>
2022-01-23 16:28:14 +0300

    Revert to connect 1

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','eventdb')or die("Could not connect to mysql".mysqli_error($con));
+//$conn= new mysqli('localhost','root','','eventdb')or die("Could not connect to mysql".mysqli_error($con));
 
 //VERSION 2
-//$conn= new mysqli('remotemysql.com','7aaTDSycGT','THD80PlERr','7aaTDSycGT')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('remotemysql.com','7aaTDSycGT','H1iYEFor7j','cOq1reHwxl')or die("Could not connect to mysql".mysqli_error($con));
